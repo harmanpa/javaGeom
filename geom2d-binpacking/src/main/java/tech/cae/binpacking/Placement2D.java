4a5be238afae2a75ee71cfd59a452580c9c7d779
Peter Harman <peter.harman@deltatheta.com>
2020-09-02 12:01:57 +0100

    WIP on tidying bin packing

--- geom2d-binpacking/src/main/java/tech/cae/binpacking/Placement2D.java
+++ geom2d-binpacking/src/main/java/tech/cae/binpacking/Placement2D.java
@@ -5,28 +5,52 @@
  */
 package tech.cae.binpacking;
 
+import math.geom2d.Point2D;
 import math.geom2d.polygon.Polygon2D;
+import math.geom2d.polygon.Polygons2D;
 
 /**
  *
  * @author peter
  */
-public class Placement2D {
+public class Placement2D implements Comparable<Placement2D> {
 
+    private final Sheet2D sheet;
     private final Polygon2D part;
     private final Polygon2D outset;
     private Polygon2D hull;
 
-    public Placement2D(Polygon2D part, Polygon2D outset) {
+    public Placement2D(Sheet2D sheet, Polygon2D part, Polygon2D outset) {
+        this.sheet = sheet;
         this.part = part;
         this.outset = outset;
     }
 
-    public Polygon2D getConvexHull(Sheet2D sheet) {
+    public Polygon2D getPart() {
+        return part;
+    }
+
+    public Polygon2D getOutset() {
+        return outset;
+    }
+
+    public Polygon2D getConvexHull() {
         if (hull == null) {
             hull = PackingUtils.augmentHull(sheet.getConvexHull(), part);
         }
         return hull;
+    }
+
+    public double getArea() {
+        Point2D centroid = Polygons2D.computeCentroid(part);
+        return getConvexHull().area()
+                * (sheet.getPreferX() * Math.abs(centroid.getX())
+                + (1 - (sheet.getPreferX()) * Math.abs(centroid.getY())));
+    }
+
+    @Override
+    public int compareTo(Placement2D o) {
+        return Double.compare(getArea(), o.getArea());
     }
 
 }
--- geom2d-binpacking/src/main/java/tech/cae/binpacking/Sheet2D.java
+++ geom2d-binpacking/src/main/java/tech/cae/binpacking/Sheet2D.java
@@ -9,6 +9,7 @@ import com.google.common.collect.Sets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import math.geom2d.Box2D;
 import math.geom2d.Point2D;
 import math.geom2d.polygon.Polygon2D;
 import math.geom2d.polygon.SimplePolygon2D;
@@ -19,31 +20,45 @@ import math.geom2d.polygon.SimplePolygon2D;
  */
 public class Sheet2D {
 
+    private final Box2D bounds;
     private final PolygonFastIntersector intersector;
     private final Set<Point2D> vertices;
     private final List<Placement2D> placements;
+    private final double preferX;
 
-    public Sheet2D() {
+    public Sheet2D(double length, double width, double preferX) {
+        this.bounds = new Box2D(0, length, 0, width);
         this.intersector = new PolygonFastIntersector();
         this.vertices = Sets.newLinkedHashSet();
         this.placements = new ArrayList<>();
+        this.preferX = preferX;
     }
 
 // size, list of placements
     public void place(Placement2D placement) {
-
+        this.placements.add(placement);
+        this.intersector.add(placement.getPart());
+        this.vertices.addAll(placement.getPart().vertices());
     }
 
     public Polygon2D getConvexHull() {
-        return placements.isEmpty() ? new SimplePolygon2D() : placements.get(placements.size() - 1).getConvexHull(this);
+        return placements.isEmpty() ? new SimplePolygon2D() : placements.get(placements.size() - 1).getConvexHull();
     }
 
     public Set<Point2D> vertices() {
         return this.vertices;
     }
 
-    public boolean feasible(Placement2D placement) {
+    public double getPreferX() {
+        return preferX;
+    }
 
+    public boolean feasible(Placement2D placement) {
+        // Is the placement actually inside the sheet?
+        if (this.bounds.containsBounds(placement.getPart())) {
+            // Does it overlap with existing parts?
+            return !this.intersector.overlaps(placement.getOutset());
+        }
         return false;
     }
 }
