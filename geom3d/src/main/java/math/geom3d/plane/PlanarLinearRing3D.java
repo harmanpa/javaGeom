806c514a5efedcee8687fd5e63fd7af11f73e909
Peter Harman <peter.harman@deltatheta.com>
2022-05-11 11:23:00 +0100

    Forgot to mark it static

--- geom3d/src/main/java/math/geom3d/plane/PlanarLinearRing3D.java
+++ geom3d/src/main/java/math/geom3d/plane/PlanarLinearRing3D.java
@@ -16,7 +16,7 @@ import math.geom3d.Point3D;
  */
 public class PlanarLinearRing3D extends PlanarShape3D<LinearRing2D> {
 
-    public PlanarLinearRing3D withDirection(List<Point3D> points, boolean cw) throws Geom2DException {
+    public static PlanarLinearRing3D withDirection(List<Point3D> points, boolean cw) throws Geom2DException {
         PlanarLinearRing3D ring = new PlanarLinearRing3D(points);
         boolean ringCW = ring.getShape().area() < 0;
         if (cw != ringCW) {
